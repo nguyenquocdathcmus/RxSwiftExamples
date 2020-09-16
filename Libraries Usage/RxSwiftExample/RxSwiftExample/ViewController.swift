10157eceae4165ad68af7229da8d773c34384f3c
Nguyen Quoc Dat <nguyenquocdat.hcmus@gmail.com>
2020-09-16 16:57:56 +0900

    update to swift 5.0

--- Libraries Usage/RxSwiftExample/RxSwiftExample/ViewController.swift
+++ Libraries Usage/RxSwiftExample/RxSwiftExample/ViewController.swift
@@ -34,9 +34,30 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var searchBar: UISearchBar!
-    
+
     var shownCities = [String]() // Data source for UITableView
-    let allCities = ["New York", "London", "Oslo", "Warsaw", "Berlin", "Praga"] // Our mocked API data source
+    let allCities = ["New York1", "London1", "Oslo1", "Warsaw1", "Berlin1", "Praga1",
+                     "New York2", "London", "Oslo", "Warsaw", "Berlin", "Praga",
+                     "New York3", "London", "Oslo", "Warsaw", "Berlin", "Praga",
+                     "New York4", "London", "Oslo", "Warsaw", "Berlin", "Praga",
+                     "New York5", "London", "Oslo", "Warsaw", "Berlin", "Praga",
+                     "New York6", "London", "Oslo", "Warsaw", "Berlin", "Praga",
+                     "New York7", "London", "Oslo", "Warsaw", "Berlin", "Praga",
+                     "New York", "London", "Oslo", "Warsaw", "Berlin", "Praga",
+                     "New York", "London", "Oslo", "Warsaw", "Berlin", "Praga",
+                     "New York", "London", "Oslo", "Warsaw", "Berlin", "Praga",
+                     "New York", "London", "Oslo", "Warsaw", "Berlin", "Praga",
+                     "New York", "London", "Oslo", "Warsaw", "Berlin", "Praga",
+                     "New York", "London", "Oslo", "Warsaw", "Berlin", "Praga",
+                     "New York", "London", "Oslo", "Warsaw", "Berlin", "Praga",
+                     "New York", "London", "Oslo", "Warsaw", "Berlin", "Praga",
+                     "New York", "London", "Oslo", "Warsaw", "Berlin", "Praga",
+                     "New York", "London", "Oslo", "Warsaw", "Berlin", "Praga",
+                     "New York", "London", "Oslo", "Warsaw", "Berlin", "Praga",
+                     "New York", "London", "Oslo", "Warsaw", "Berlin", "Praga",
+                     "New York", "London", "Oslo", "Warsaw", "Berlin", "Praga",
+                     "New York", "London", "Oslo", "Warsaw", "Berlin", "Praga",
+                     ] // Our mocked API data source
     let disposeBag = DisposeBag() // Bag of disposables to release them when view is being deallocated
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -45,17 +66,15 @@ class ViewController: UIViewController {
     
     func setup() {
         tableView.dataSource = self
-        searchBar
-            .rx.text // Observable property thanks to RxCocoa
-            .orEmpty // Make it non-optional
-            .debounce(0.5, scheduler: MainScheduler.instance) // Wait 0.5 for changes.
+        searchBar.rx.text.orEmpty
+            .throttle(0.5, scheduler: MainScheduler.instance) // Wait 0.5 for changes.
             .distinctUntilChanged() // If they didn't occur, check if the new value is the same as old.
             .filter { !$0.isEmpty } // If the new value is really new, filter for non-empty query.
             .subscribe(onNext: { [unowned self] query in // Here we subscribe to every new value, that is not empty (thanks to filter above).
                 self.shownCities = self.allCities.filter { $0.hasPrefix(query) } // We now do our "API Request" to find cities.
                 self.tableView.reloadData() // And reload table view data.
             })
-            .addDisposableTo(disposeBag) // Don't forget to add this to disposeBag. We want to dispose it on deinit.
+            .disposed(by: disposeBag) // Don't forget to add this to disposeBag. We want to dispose it on deinit.
     }
     
 }
